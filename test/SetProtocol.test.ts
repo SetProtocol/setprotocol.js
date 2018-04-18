1678fb0bf49a3812174561da086eb3a11883da8f
Justin Chen <justinkchen14@gmail.com>
2018-04-17 22:57:17 -0700

    Add in testing skeleton

--- test/SetProtocol.test.ts
+++ test/SetProtocol.test.ts
@@ -1,1 +1,11 @@
+import * as chai from "chai";
+
+const { expect } = chai;
+
 // SetProtocol.test.ts
+describe(`SetProtocol`, () => {
+  // TODO: Add more tests
+  it(`should run test suite`, () => {
+    expect(1).to.equal(1);
+  });
+});
