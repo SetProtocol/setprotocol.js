55ef8c0c2d697b1ee7cfcbf139a5bd1fb2551d41
Felix <felix2feng@gmail.com>
2018-05-14 19:00:12 -0700

    Use smarter

--- src/util/set_token_utils.ts
+++ src/util/set_token_utils.ts
@@ -3,5 +3,10 @@ import { BigNumber } from "./bignumber";
 const TRANSFER_GAS_MAXIMUM = new BigNumber(70000);
 
 export const estimateIssueRedeemGasCost = (numComponents: BigNumber): number => {
-  return numComponents.times(TRANSFER_GAS_MAXIMUM).toNumber();
+  const baseIssueRedeemGasCost = new BigNumber(70000);
+
+  return numComponents
+    .times(TRANSFER_GAS_MAXIMUM)
+    .plus(baseIssueRedeemGasCost)
+    .toNumber();
 };
