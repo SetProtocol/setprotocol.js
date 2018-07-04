388563e6bd572a25a02059559ca1c1c7960de2ce
inje <byeo630@gmail.com>
2018-07-03 21:11:06 -0700

    Index: Remove cruft from v1

--- src/index.ts
+++ src/index.ts
@@ -19,24 +19,25 @@
 import * as Web3 from "web3";
 
 import { CoreAPI } from "./api";
-import { Config } from "./types/common";
 
 /**
- * The SetProtocol class is the single entry-point into the SetProtocol library.
- * It contains all of the library's functionality
- * and all calls to the library should be made through a SetProtocol instance.
+ * @title SetProtocol
+ * @author Set Protocol
+ *
+ * The SetProtocol class that exposes all functionality for interacting with the SetProtocol smart contracts.
+ * Methods that require interaction with the Ethereum blockchain are exposed after instantiating a new instance
+ * of SetProtocol with the web3 provider argument
  */
 export default class SetProtocol {
-  private provider: Web3; // A property storing the Web3.js Provider instance
-  public v1: SetProtocolV1;
+  private provider: Web3;
+  public setCore: CoreAPI;
 
   /**
    * Instantiates a new SetProtocol instance that provides the public interface to the SetProtocol.js library.
    * @param   provider    The Web3.js Provider instance you would like the SetProtocol.js library to use for interacting with
    *                      the Ethereum network.
-   * @param   config     Config object that allow setProtocol.js to be exposed differently
    */
-  constructor(provider: Web3 = undefined, config: Config = {}) {
+  constructor(provider: Web3 = undefined) {
     this.provider = provider;
     this.setCore = new CoreAPI(this.provider);
   }
