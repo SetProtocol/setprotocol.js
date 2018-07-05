003288353aba213c22a4cfe489001b5401a6ece3
Justin Chen <justinkchen14@gmail.com>
2018-07-04 19:42:44 -0700

    Delete versioned __mocks__ folder in favor of parent-level global folder

--- _versions/v1/__mocks__/set-protocol-contracts.ts
+++
@@ -1,6 +0,0 @@
-import { MockContract } from "./MockContract";
-
-export class ERC20 extends MockContract {};
-export class DetailedERC20 extends MockContract {};
-export class SetTokenRegistry extends MockContract {};
-export class SetToken extends MockContract {};
