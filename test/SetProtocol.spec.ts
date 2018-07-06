7a2b9d273a03b7c3456209c477a9d9937deb7e45
inje <byeo630@gmail.com>
2018-07-05 19:14:17 -0700

    Test: Clean up setProtocol test

--- test/SetProtocol.spec.ts
+++ test/SetProtocol.spec.ts
@@ -18,13 +18,12 @@
 
 import * as chai from "chai";
 import * as Web3 from "web3";
+
 import SetProtocol from '../src';
 
 const provider = new Web3.providers.HttpProvider("http://localhost:8545");
-
 const { expect } = chai;
 
-// SetProtocol.test.ts
 describe(`SetProtocol`, () => {
   const setProtocolInstance = new SetProtocol(provider);
   it(`should instantiate a new setProtocolInstance`, () => {
