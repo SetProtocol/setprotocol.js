ce6b4f8a34300e22af08e5ae78081ad49e609ae6
Justin Chen <justinkchen14@gmail.com>
2018-07-05 21:24:07 -0700

    Fix missing ending brackets

--- test/integration/CoreAPI.spec.ts
+++ test/integration/CoreAPI.spec.ts
@@ -20,4 +20,5 @@ describe("Core API", () => {
   /* Remove this test() after adding first test. Yarn test fails without at least 1 test */
   test("Initialize Core", async () => {
     expect(true);
+  });
 });
