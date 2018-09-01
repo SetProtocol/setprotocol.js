2db0496a7054c8eb3c701579af8fc7c59f63c644
inje <byeo630@gmail.com>
2018-09-01 13:54:21 -0700

    Chain: Fix yarn chain command pathing from developer's cwd

---
+++ scripts/init_chain_internal.sh
@@ -0,0 +1,7 @@
+# #!/bin/bash
+
+SET_PROTOCOL_JS=`pwd`
+
+bash $SET_PROTOCOL_JS/scripts/migrate_set_protocol_contracts.sh &
+
+ganache-cli --db blockchain --networkId 50 --accounts 20 -l 10000000 -e 10000000000 --mnemonic 'concert load couple harbor equip island argue ramp clarify fence smart topic'
