c13d7c4c44d4ca16f3ebecb8a02e5b8414a0c369
Alex Soong <alexsoong91@gmail.com>
2018-09-02 18:34:24 -0700

    ER20API (#133)
    * ER20API
    
    * Move around init
    
    * Update comment

--- src/SetProtocol.ts
+++ src/SetProtocol.ts
@@ -21,8 +21,8 @@ import { TransactionReceipt } from 'ethereum-types';
 import { Address, Bytes, SetProtocolUtils } from 'set-protocol-utils';
 import { Provider } from '@0xproject/types';
 
-import { AccountingAPI, BlockchainAPI, FactoryAPI, IssuanceAPI, OrderAPI, SetTokenAPI } from './api';
-import { CoreWrapper, ERC20Wrapper, VaultWrapper } from './wrappers';
+import { AccountingAPI, BlockchainAPI, ERC20API, FactoryAPI, IssuanceAPI, OrderAPI, SetTokenAPI } from './api';
+import { CoreWrapper, VaultWrapper } from './wrappers';
 import { BigNumber, IntervalManager, instantiateWeb3 } from './util';
 import { TxData, TxDataWithFrom } from './types/common';
 
@@ -62,9 +62,9 @@ class SetProtocol {
   public orders: OrderAPI;
 
   /**
-   * An instance of the ERC20Wrapper class containing methods for interacting with ERC20 compliant contracts
+   * An instance of the ERC20API class containing methods for interacting with ERC20 compliant contracts
    */
-  public erc20: ERC20Wrapper;
+  public erc20: ERC20API;
 
   /**
    * An instance of the SetTokenAPI class containing methods for interacting with SetToken contracts
@@ -81,10 +81,10 @@ class SetProtocol {
     this.web3 = instantiateWeb3(provider);
 
     this.core = new CoreWrapper(this.web3, config.coreAddress, config.transferProxyAddress, config.vaultAddress);
-    this.erc20 = new ERC20Wrapper(this.web3);
     this.vault = new VaultWrapper(this.web3, config.vaultAddress);
 
     this.accounting = new AccountingAPI(this.web3, this.core);
+    this.erc20 = new ERC20API(this.web3);
     this.factory = new FactoryAPI(this.web3, this.core, config.setTokenFactoryAddress);
     this.issuance = new IssuanceAPI(this.web3, this.core);
     this.orders = new OrderAPI(this.web3, this.core);
