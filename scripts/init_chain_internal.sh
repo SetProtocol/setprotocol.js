0d7184e5e309ff4b3efaf4356be98b965369b55a
Alexander Soong <alexsoong91@gmail.com>
2018-09-04 12:22:15 -0700

    Add set-protocol snapshot and update chain configs

--- scripts/init_chain_internal.sh
+++ scripts/init_chain_internal.sh
@@ -2,6 +2,6 @@
 
 SET_PROTOCOL_JS=`pwd`
 
-bash $SET_PROTOCOL_JS/scripts/migrate_set_protocol_contracts.sh &
+rm -rf blockchain && cp -r snapshots/0x blockchain
 
 ganache-cli --db blockchain --networkId 50 --accounts 20 -l 10000000 -e 10000000000 --mnemonic 'concert load couple harbor equip island argue ramp clarify fence smart topic'
