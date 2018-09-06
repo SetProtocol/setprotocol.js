a3e8118b127762cb2135870fc10a4614b365c9ad
Justin Chen <justin@setprotocol.com>
2018-09-06 00:28:23 -0700

    Remove package

--- package/src/util/units.ts
+++
@@ -1,9 +0,0 @@
-import * as Web3 from 'web3';
-import { BigNumber } from 'bignumber.js';
-
-const web3 = new Web3();
-
-export function ether(amount: number): BigNumber {
-  const weiString = web3.toWei(amount, 'ether');
-  return new BigNumber(weiString);
-}
