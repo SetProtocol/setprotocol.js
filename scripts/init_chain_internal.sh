4cf96dfbd56bd63496b9a05780e18b7879cd3ea4
Felix Feng <felix2feng@gmail.com>
2018-09-07 12:52:01 +0200

    Use set vs 0x chain (#154)

--- scripts/init_chain_internal.sh
+++ scripts/init_chain_internal.sh
@@ -2,6 +2,6 @@
 
 SET_PROTOCOL_JS=`pwd`
 
-rm -rf blockchain && cp -r snapshots/0x blockchain
+rm -rf blockchain && cp -r snapshots/set_protocol blockchain
 
 ganache-cli --db blockchain --networkId 50 --accounts 20 -l 10000000 -e 10000000000 --mnemonic 'concert load couple harbor equip island argue ramp clarify fence smart topic'
