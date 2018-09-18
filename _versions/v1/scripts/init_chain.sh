22b29a3bc5bd81b4c72bd6e766cf34407f55ff34
Justin K. Chen <justin@setprotocol.com>
2018-09-18 09:19:54 -0700

    Remove _versions folder (#168)

--- _versions/v1/scripts/init_chain.sh
+++
@@ -1,7 +0,0 @@
-
-# #!/bin/bash
-SET_PROTOCOL_JS_REPO=`pwd`
-
-bash $SET_PROTOCOL_JS_REPO/scripts/migrate_set_protocol_contracts.sh &
-
-ganache-cli --networkId 70 --accounts 20 --mnemonic 'candy maple cake sugar pudding cream honey rich booboo crumble sweet treat'
--- _versions/v1/src/invariants/common.ts
+++
@@ -1,9 +0,0 @@
-import { BigNumber } from "../util/bignumber";
-
-export class CommonAssertions {
-  public greaterThanZero(quantity: BigNumber, errorMessage: string) {
-    if (quantity.lte(new BigNumber(0))) {
-      throw new Error(errorMessage);
-    }
-  }
-}
