5c3982861e28d5561795e6d46ce589fd419261c9
felix2feng <felix2feng@gmail.com>
2019-01-31 12:32:42 -0800

    Work on api

---
+++ src/api/RebalancingManagerAPI.ts
@@ -0,0 +1,69 @@
+/*
+  Copyright 2018 Set Labs Inc.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+'use strict';
+
+import * as _ from 'lodash';
+import Web3 from 'web3';
+import { SetTokenContract, VaultContract } from 'set-protocol-contracts';
+
+import { ZERO } from '../constants';
+import { coreAPIErrors, erc20AssertionErrors, vaultAssertionErrors } from '../errors';
+import { Assertions } from '../assertions';
+import { CoreWrapper, BTCETHRebalancingManagerWrapper } from '../wrappers';
+import { BigNumber } from '../util';
+import { Address, Tx } from '../types/common';
+
+/**
+ * @title RebalancingManagerAPI
+ * @author Set Protocol
+ *
+ * A library for issuing and redeeming Sets
+ */
+export class RebalancingManagerAPI {
+  private web3: Web3;
+  private assert: Assertions;
+  private core: CoreWrapper;
+  private btcEthRebalancingManager: BTCETHRebalancingManagerWrapper;
+
+  /**
+   * Instantiates a new RebalancingManagerAPI instance that contains methods for issuing and redeeming Sets
+   *
+   * @param web3        The Web3.js Provider instance you would like the SetProtocol.js library to use for interacting
+   *                      with the Ethereum network
+   * @param core        An instance of CoreWrapper to interact with the deployed Core contract
+   * @param assertions  An instance of the Assertion library
+   */
+  constructor(web3: Web3, core: CoreWrapper, assertions: Assertions) {
+    this.web3 = web3;
+    this.core = core;
+    this.assert = assertions;
+  }
+
+  /**
+   * Issues a Set to the transaction signer. Must have component tokens in the correct quantites in either
+   * the vault or in the signer's wallet. Component tokens must be approved to the Transfer
+   * Proxy contract via setTransferProxyAllowanceAsync
+   *
+   * @param  setAddress    Address Set to issue
+   * @param  quantity      Amount of Set to issue. Must be multiple of the natural unit of the Set
+   * @param  txOpts        Transaction options object conforming to `Tx` with signer, gas, and gasPrice data
+   * @return               Transaction hash
+   */
+  public async proposeAsync(rebalancingManagerAddress: Address, rebalancingSetAddress: Address, txOpts: Tx): Promise<string> {
+    return await this.core.issue(setAddress, quantity, txOpts);
+  }
+}
--- test/integration/api/RebalancingManagerAPI.spec.ts
+++ test/integration/api/RebalancingManagerAPI.spec.ts
@@ -33,7 +33,6 @@ import {
   BTCETHRebalancingManagerContract,
   CoreContract,
   ConstantAuctionPriceCurveContract,
-  IssuanceOrderModuleContract,
   MedianContract,
   SetTokenContract,
   RebalanceAuctionModuleContract,
@@ -60,27 +59,21 @@ import {
 import { Assertions } from '@src/assertions';
 import {
   addPriceCurveToCoreAsync,
-  addPriceFeedOwnerToMedianizer,
   addWhiteListedTokenAsync,
   approveForTransferAsync,
   constructInflowOutflowArraysAsync,
   createDefaultRebalancingSetTokenAsync,
   deployBaseContracts,
-  deployBtcEthManagerContractAsync,
   deployConstantAuctionPriceCurveAsync,
   deployCoreContract,
   deployKyberNetworkWrapperContract,
   deploySetTokenAsync,
-  deployMedianizerAsync,
   deploySetTokensAsync,
   deployTakerWalletWrapperContract,
   deployTokenAsync,
   deployTokensAsync,
-  deployTokensSpecifyingDecimals,
   deployZeroExExchangeWrapperContract,
   getVaultBalances,
-  increaseChainTimeAsync,
-  updateMedianizerPriceAsync,
   tokenDeployedOnSnapshot,
   transitionToRebalanceAsync,
 } from '@test/helpers';
@@ -116,7 +109,6 @@ describe('BTCETHRebalancingManagerWrapper', () => {
   let core: CoreContract;
   let transferProxy: TransferProxyContract;
   let vault: VaultContract;
-  let issuanceOrderModule: IssuanceOrderModuleContract;
   let rebalanceAuctionModule: RebalanceAuctionModuleContract;
   let factory: SetTokenFactoryContract;
   let rebalancingFactory: RebalancingSetTokenFactoryContract;
@@ -127,12 +119,6 @@ describe('BTCETHRebalancingManagerWrapper', () => {
   let ethMedianizer: MedianContract;
   let wrappedBTC: StandardTokenMockContract;
   let wrappedETH: StandardTokenMockContract;
-  let whitelist: WhiteListContract;
-
-  let btcMultiplier: BigNumber;
-  let ethMultiplier: BigNumber;
-
-  let btcEthManagerWrapper: BTCETHRebalancingManagerWrapper;
 
   beforeAll(() => {
     ABIDecoder.addABI(coreContract.abi);
@@ -149,58 +135,27 @@ describe('BTCETHRebalancingManagerWrapper', () => {
       core,
       transferProxy,
       vault,
-      factory,
-      rebalancingFactory,
+      setTokenFactory,
+      rebalancingSetTokenFactory,
       rebalanceAuctionModule,
       issuanceOrderModule,
       whitelist,
     ] = await deployBaseContracts(web3);
 
-    btcMedianizer = await deployMedianizerAsync(web3);
-    await addPriceFeedOwnerToMedianizer(btcMedianizer, DEFAULT_ACCOUNT);
-    ethMedianizer = await deployMedianizerAsync(web3);
-    await addPriceFeedOwnerToMedianizer(ethMedianizer, DEFAULT_ACCOUNT);
+    btcMedianizer = await deployMedianizerAsync();
+    await addPriceFeedOwnerToMedianizer(btcMedianizer, deployerAccount);
+    ethMedianizer = await deployMedianizerAsync();
+    await addPriceFeedOwnerToMedianizer(ethMedianizer, deployerAccount);
 
-    [wrappedBTC, wrappedETH] = await deployTokensSpecifyingDecimals(2, [8, 18], web3, DEFAULT_ACCOUNT);
-    await approveForTransferAsync(
+    wrappedBTC = await erc20Wrapper.deployTokenAsync(deployerAccount, 8);
+    wrappedETH = await erc20Wrapper.deployTokenAsync(deployerAccount, 18);
+    await erc20Wrapper.approveTransfersAsync(
       [wrappedBTC, wrappedETH],
       transferProxy.address
     );
-    await addWhiteListedTokenAsync(
-      whitelist,
-      wrappedBTC.address,
-    );
-    await addWhiteListedTokenAsync(
-      whitelist,
-      wrappedETH.address,
-    );
-
-    constantAuctionPriceCurve = await deployConstantAuctionPriceCurveAsync(
-      web3,
-      DEFAULT_AUCTION_PRICE_NUMERATOR,
-      DEFAULT_AUCTION_PRICE_DENOMINATOR,
-    );
-
-    addPriceCurveToCoreAsync(
-      core,
-      constantAuctionPriceCurve.address,
-    );
-
-    btcMultiplier = new BigNumber(1);
-    ethMultiplier = new BigNumber(1);
-
-    btcethRebalancingManager = await deployBtcEthManagerContractAsync(
-      web3,
-      core.address,
-      btcMedianizer.address,
-      ethMedianizer.address,
-      wrappedBTC.address,
-      wrappedETH.address,
-      factory.address,
-      constantAuctionPriceCurve.address,
-      ONE_DAY_IN_SECONDS,
-      btcMultiplier,
-      ethMultiplier,
+    await coreWrapper.addTokensToWhiteList(
+      [wrappedBTC.address, wrappedETH.address],
+      rebalancingComponentWhiteList
     );
 
     btcEthManagerWrapper = new BTCETHRebalancingManagerWrapper(
@@ -212,78 +167,80 @@ describe('BTCETHRebalancingManagerWrapper', () => {
     await web3Utils.revertToSnapshot(currentSnapshotId);
   });
 
-  describe('propose', async () => {
-    let rebalancingSetToken: RebalancingSetTokenContract;
-    
-    let proposalPeriod: BigNumber;
-    let btcPrice: BigNumber;
-    let ethPrice: BigNumber;
-    let ethUnit: BigNumber;
+  // describe('propose', async () => {
+  //   let rebalancingSetToken: RebalancingSetTokenContract;
+  //   let currentSetToken: SetTokenContract;
+  //   let nextSetToken: SetTokenContract;
 
-    let initialAllocationToken: SetTokenContract;
-    let timeFastForward: BigNumber;
+  //   let subjectRebalancingSetToken: Address;
+  //   let subjectBidQuantity: BigNumber;
+  //   let subjectCaller: Address;
 
-    let subjectRebalancingSetToken: Address;
-    let subjectManagerAddress: Address;
-    let subjectCaller: Address;
+  //   beforeEach(async () => {
+  //     const setTokens = await deploySetTokensAsync(
+  //       web3,
+  //       core,
+  //       setTokenFactory.address,
+  //       transferProxy.address,
+  //       2,
+  //     );
 
-    beforeAll(async () => {
-      btcPrice = new BigNumber(4082 * 10 ** 18);
-      ethPrice = new BigNumber(128 * 10 ** 18);
-      ethUnit = new BigNumber(28.999 * 10 ** 10);
-    });
+  //     currentSetToken = setTokens[0];
+  //     nextSetToken = setTokens[1];
 
-    beforeEach(async () => {
-      initialAllocationToken = await deploySetTokenAsync(
-        web3,
-        core,
-        factory.address,
-        [wrappedBTC.address, wrappedETH.address],
-        [new BigNumber(1).mul(btcMultiplier), ethUnit.mul(ethMultiplier)],
-        new BigNumber(10 ** 10),
-      );
+  //     const proposalPeriod = ONE_DAY_IN_SECONDS;
+  //     const managerAddress = ACCOUNTS[1].address;
+  //     rebalancingSetToken = await createDefaultRebalancingSetTokenAsync(
+  //       web3,
+  //       core,
+  //       rebalancingSetTokenFactory.address,
+  //       managerAddress,
+  //       currentSetToken.address,
+  //       proposalPeriod
+  //     );
 
-      proposalPeriod = ONE_DAY_IN_SECONDS;
-      rebalancingSetToken = await createDefaultRebalancingSetTokenAsync(
-        web3,
-        core,
-        rebalancingFactory.address,
-        btcethRebalancingManager.address,
-        initialAllocationToken.address,
-        proposalPeriod
-      );
+  //     // Issue currentSetToken
+  //     await core.issue.sendTransactionAsync(currentSetToken.address, ether(9), TX_DEFAULTS);
+  //     await approveForTransferAsync([currentSetToken], transferProxy.address);
 
-      timeFastForward = ONE_DAY_IN_SECONDS.add(1);
-      await updateMedianizerPriceAsync(
-        web3,
-        btcMedianizer,
-        btcPrice,
-        SetTestUtils.generateTimestamp(1000),
-      );
+  //     // Use issued currentSetToken to issue rebalancingSetToken
+  //     const rebalancingSetQuantityToIssue = ether(7);
+  //     await core.issue.sendTransactionAsync(rebalancingSetToken.address, rebalancingSetQuantityToIssue);
 
-      await updateMedianizerPriceAsync(
-        web3,
-        ethMedianizer,
-        ethPrice,
-        SetTestUtils.generateTimestamp(1000),
-      );
+  //     // Approve proposed Set's components to the whitelist;
+  //     const [proposalComponentOne, proposalComponentTwo] = await nextSetToken.getComponents.callAsync();
+  //     await addWhiteListedTokenAsync(whitelist, proposalComponentOne);
+  //     await addWhiteListedTokenAsync(whitelist, proposalComponentTwo);
 
-      subjectManagerAddress = btcethRebalancingManager.address;
-      subjectRebalancingSetToken = rebalancingSetToken.address;
-      subjectCaller = DEFAULT_ACCOUNT;
-    });
+  //     // Deploy price curve used in auction
+  //     const priceCurve = await deployConstantAuctionPriceCurveAsync(
+  //       web3,
+  //       DEFAULT_AUCTION_PRICE_NUMERATOR,
+  //       DEFAULT_AUCTION_PRICE_DENOMINATOR
+  //     );
 
-    async function subject(): Promise<string> {
-      await increaseChainTimeAsync(web3, timeFastForward);
-      return await btcEthManagerWrapper.propose(
-        subjectManagerAddress,
-        subjectRebalancingSetToken,
-        { from: subjectCaller },
-      );
-    }
+  //     addPriceCurveToCoreAsync(
+  //       core,
+  //       priceCurve.address
+  //     );
 
-    test('successfully proposes', async () => {
-      await subject();
-    });
-  });
+
+
+  //     subjectRebalancingSetToken = rebalancingSetToken.address;
+  //     subjectBidQuantity = ether(2);
+  //     subjectCaller = DEFAULT_ACCOUNT;
+  //   });
+
+  //   async function subject(): Promise<string> {
+  //     return await btcEthManagerWrapper.propose(
+  //       'wrapper contract address',
+  //       subjectRebalancingSetToken,
+  //       { from: subjectCaller },
+  //     );
+  //   }
+
+  //   test('successfully proposes', async () => {
+  //     await subject();
+  //   });
+  // });
 });
