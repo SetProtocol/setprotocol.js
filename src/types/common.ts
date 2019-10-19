4467b977534627bcb2eb86d9a6a2040f6c4bd6b4
bweick <brian.weickmann@gmail.com>
2019-10-19 12:44:54 +0900

    Added timestamp to Bid Event getter. (#326)
    * Added timestamp to Bid Event getter.
    
    * Bump package

--- src/types/common.ts
+++ src/types/common.ts
@@ -23,6 +23,7 @@ export interface BidPlacedEvent {
   combinedTokenAddresses: Address[];
   inflowTokenUnits: BigNumber[];
   outflowTokenUnits: BigNumber[];
+  timestamp: number;
 }
 
 export interface Component {
