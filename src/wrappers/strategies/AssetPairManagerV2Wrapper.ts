b7e168bb6e0618ca8f02f9ef550a2e00a1b48861
Richard Liang <richardliang2015@u.northwestern.edu>
2020-05-12 11:50:06 -0700

    Remove unused wrapper functions

--- src/wrappers/strategies/AssetPairManagerV2Wrapper.ts
+++ src/wrappers/strategies/AssetPairManagerV2Wrapper.ts
@@ -39,42 +39,6 @@ export class AssetPairManagerV2Wrapper {
   }
 
   /**
-   * Calls an AssetPairManagerV2's intialPropose function. This function kicks off a propose cycle on the
-   * manager by checking that the time and price constraints have been met then logging a timestamp used later in the
-   * process to confirm the signal.
-   *
-   * @param  managerAddress               Address of the rebalancing manager contract
-   * @return                              The hash of the resulting transaction.
-   */
-  public async initialPropose(
-    managerAddress: Address,
-    txOpts?: Tx,
-  ): Promise<string> {
-    const assetPairManagerInstance = await this.contracts.loadAssetPairManagerV2ContractAsync(managerAddress);
-    const txOptions = await generateTxOpts(this.web3, txOpts);
-
-    return await assetPairManagerInstance.initialPropose.sendTransactionAsync(txOptions);
-  }
-
-  /**
-   * Calls an AssetPairManagerV2's confirmPropose function. This function again checks to make sure that
-   * price and time constraints have been satisfied. After that it generates the new allocation proposal and sends the
-   * results to the Rebalancing Set Token to kick off the official rebalance.
-   *
-   * @param  managerAddress               Address of the rebalancing manager contract
-   * @return                              The hash of the resulting transaction.
-   */
-  public async confirmPropose(
-    managerAddress: Address,
-    txOpts?: Tx,
-  ): Promise<string> {
-    const assetPairManagerInstance = await this.contracts.loadAssetPairManagerV2ContractAsync(managerAddress);
-    const txOptions = await generateTxOpts(this.web3, txOpts);
-
-    return await assetPairManagerInstance.confirmPropose.sendTransactionAsync(txOptions);
-  }
-
-  /**
    * Update liquidator used by Rebalancing Set.
    *
    * @param  managerAddress               Address of the rebalancing manager contract
@@ -168,32 +132,6 @@ export class AssetPairManagerV2Wrapper {
       upgradeHash,
       txOptions
     );
-  }
-
-  /**
-   * Calls an AssetPairManagerV2's canInitialPropose function. Returns whether initialPropose can be called
-   * without reverting.
-   *
-   * @param  managerAddress               Address of the rebalancing manager contract
-   * @return                              Boolean indicating whether initialPropose can be successfully called
-   */
-  public async canInitialPropose(managerAddress: Address): Promise<boolean> {
-    const assetPairManagerInstance = await this.contracts.loadAssetPairManagerV2ContractAsync(managerAddress);
-
-    return await assetPairManagerInstance.canInitialPropose.callAsync();
-  }
-
-  /**
-   * Calls an AssetPairManagerV2's canConfirmPropose function. Returns whether confirmPropose can be called
-   * without reverting.
-   *
-   * @param  managerAddress               Address of the rebalancing manager contract
-   * @return                              Boolean indicating whether confirmPropose can be successfully called
-   */
-  public async canConfirmPropose(managerAddress: Address): Promise<boolean> {
-    const assetPairManagerInstance = await this.contracts.loadAssetPairManagerV2ContractAsync(managerAddress);
-
-    return await assetPairManagerInstance.canConfirmPropose.callAsync();
   }
 
   public async core(managerAddress: Address): Promise<Address> {
